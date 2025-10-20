b61c2c3b7bddb32632e8ffeb23f7a49a0aadbd65
alexpodelinskii <nikitik007@yandex.ru>
2025-10-21 00:52:15 +0300

    refactor: function pureOnBlur add logic and props types

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,24 +1,28 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
-import { UserType } from './HW3'
+import {UserType} from './HW3'
 
 type GreetingContainerPropsType = {
     users: UserType[] // need to fix any
     addUserCallback: string // need to fix any
 }
 
-export const pureAddUser = (name: string, setError: (error:string)=>void, setName: (newName:string)=>void, addUserCallback: (newName:string)=>void) => {
+export const pureAddUser = (name: string, setError: (error: string) => void, setName: (newName: string) => void, addUserCallback: (newName: string) => void) => {
     // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
     const newName = name.trim();
-    if(newName.length){
+    if (newName.length) {
         addUserCallback(newName)
         setName('')
-    } else{
+    } else {
         setError('Ошибка! Введите имя!')
     }
 }
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+export const pureOnBlur = (name: string, setError: (error: string) => void) => { // если имя пустое - показать ошибку
+    const newName = name.trim();
+    if (!name.trim().length) {
+        setError('Ошибка! Введите имя!')
+    }
 }
 
 export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
@@ -29,9 +33,9 @@ export const pureOnEnter = (e: any, addUser: any) => { // если нажата 
 
 // более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
     // деструктуризация пропсов
     const [name, setName] = useState<any>('') // need to fix any
     const [error, setError] = useState<any>('') // need to fix any
